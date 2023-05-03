2008e705d23e16d5187ca5ba41f0515cbbd03fee
zzy50 <jiyeong3141592@gmail.com>
2023-05-03 10:26:18 +0900

    00

---
+++ open_test.py
@@ -0,0 +1,27 @@
+# import pickle
+# import npy_append_array
+
+# PICKLE_PATH = "test.bin"
+
+# def generate_list():
+#     for i in range(0, 100, 4):
+#         yield [i, i+1, i+2, i+3] 
+
+
+# for list1 in generate_list():
+#     # print(list1)
+#     with open(PICKLE_PATH, "a+b") as abf:
+#         pickle.dump(list1, abf, pickle.HIGHEST_PROTOCOL)
+
+
+# with open(PICKLE_PATH, "rb") as rbf:
+#     loaded_list = pickle.load(rbf)
+#     print(loaded_list)
+
+
+PATH = "path_list.txt"
+with open(PATH, "r") as f:
+    abc = f.readlines()
+    print(abc)
+
+print(abc)
---
+++ open_test1.py
@@ -0,0 +1,7 @@
+
+PATH = "path_list.txt"
+with open(PATH, "r") as f:
+    abc = f.readlines()
+    print(abc)
+
+print(abc)
